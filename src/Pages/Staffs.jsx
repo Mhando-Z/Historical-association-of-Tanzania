c8340848e91164c6c3a8cdfa88fd30fe7afb0c0c
Mhando-Z <Mhandosz17@gmail.com>
2024-08-12 12:20:53 +0300

    Phase One

--- src/Pages/Staffs.jsx
+++ src/Pages/Staffs.jsx
@@ -8,8 +8,15 @@ function Staffs() {
   return (
     <div className="container relative flex min-h-screen px-6 mx-auto mt-20">
       <div className="flex flex-col ">
-        <h1 className="xl:text-6xl md:text-5xl text-4xl font-extrabold mb-12 text-[#b67a3d]">
+        <h1 className="md:text-5xl flex flex-col text-4xl font-extrabold mb-7 text-[#b67a3d]">
           Our Team
+          <span className="max-w-xl mt-3 text-base font-normal text-gray-800 xl:text-lg">
+            Our team is a group of passionate professionals dedicated to
+            delivering top-notch services and solutions. Each of us brings
+            unique skills and expertise, working together to ensure the best
+            experience for our clients. Meet the team behind our success and
+            discover what makes us tick!
+          </span>
         </h1>
         <ImageList data={StaffsSect} />
       </div>
