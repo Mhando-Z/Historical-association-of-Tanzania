bd90b0e0d2625baa5393f670358fda4005c0f7cf
Mhando-Z <Mhandosz17@gmail.com>
2024-08-28 00:32:05 +0300

    Phase Two

--- src/Screens/HATPresident.jsx
+++ src/Screens/HATPresident.jsx
@@ -1,32 +1,65 @@
 import React, { useContext } from "react";
 import HomePageContext from "../Context/HomePageContext";
+import { motion } from "framer-motion";
 
 function HATPresident() {
   const { PresidentSect } = useContext(HomePageContext);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen mb-10">
-      <div className="relative w-full overflow-hidden aspect-h-1 aspect-w-1 xl:aspect-h-8 xl:aspect-w-7">
-        <img
-          src={`http://127.0.0.1:8000/${PresidentSect[0]?.image2}`}
-          alt={PresidentSect[0]?.subtitle}
-          className="h-[650px] w-full object-cover object-top  transition-all duration-500 ease-in"
-        />
-        <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b from-transparent via-transparent via-90% to-white"></div>
-      </div>
-      <div className="container flex flex-col mx-auto xl:mt-10 gap-y-3">
-        <h1 className="max-w-2xl text-5xl font-bold tracking-tighter text-gray-800 xl:text-6xl">
-          {PresidentSect[0]?.title}
-        </h1>
-        <h2 className="font-semibold text-gray-700 lg:text-3xl">
-          {PresidentSect[0]?.subtitle}
-        </h2>
-        <div className="">
-          <div>
-            <p className="tracking-tighter text-justify xl:text-lg">
-              {PresidentSect[0]?.description}
-            </p>
-          </div>
+    <div className="flex flex-col min-h-screen mt-24">
+      <div className="container flex flex-col mx-auto gap-y-5 gap-x-10 md:flex-row">
+        {/* Basic Info */}
+        <div className="flex flex-col shadow">
+          {/* image */}
+          <motion.img
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1 }}
+            src={`http://127.0.0.1:8000/${PresidentSect[0]?.image}`}
+            alt={PresidentSect[0]?.subtitle}
+            className="max-w-screen object-top rounded-xl object-cover h-[450px]"
+          />
+        </div>
+        {/* description */}
+        <div className="flex flex-col flex-grow gap-y-2 ">
+          <motion.h1
+            initial={{ opacity: 0, y: 20 }}
+            viewport={{ once: true }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.3 }}
+            className="px-2 text-xl font-medium border-l-4 border-l-[#744517] xl:text-4xl"
+          >
+            {PresidentSect[0]?.title}
+          </motion.h1>
+          <motion.h2
+            initial={{ opacity: 0, y: 20 }}
+            viewport={{ once: true }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.3 }}
+            className="px-2 text-xl font-medium border-l-4 border-l-[#744517] xl:text-2xl"
+          >
+            {PresidentSect[0]?.subtitle}
+          </motion.h2>
+          {/* Description */}
+          <motion.p
+            initial={{ opacity: 0, y: 20 }}
+            viewport={{ once: true }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.8 }}
+            className="max-w-5xl tracking-tighter text-justify"
+          >
+            {PresidentSect[0]?.description}
+          </motion.p>
+          <motion.img
+            initial={{ opacity: 0, y: 20 }}
+            viewport={{ once: true }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 1 }}
+            src={`http://127.0.0.1:8000/${PresidentSect[0]?.image2}`}
+            alt={PresidentSect[0]?.subtitle}
+            className="max-w-screen opacity-70 object-top object-cover mt-2 mb-20 rounded-xl h-[450px]"
+          />
         </div>
       </div>
     </div>
