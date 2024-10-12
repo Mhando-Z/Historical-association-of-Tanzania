90a26c8a77a3f9bb423e643a44782b3cf630ba06
Mhando-Z <Mhandosz17@gmail.com>
2024-10-12 05:52:04 +0300

    Phase One

--- src/Pages/PresidentSpeech.jsx
+++ src/Pages/PresidentSpeech.jsx
@@ -16,6 +16,11 @@ function PresidentSpeech() {
   // image url
   const IMAGE_BASE_URL = "https://hat-dashboard.onrender.com";
 
+  // Fallback UI when there are no data
+  if (!PresidentSect || PresidentSect?.length === 0) {
+    return <div className="bg-white"></div>;
+  }
+
   return (
     <div
       className={`relative px-6 ${
@@ -32,6 +37,7 @@ function PresidentSpeech() {
               <motion.div
                 initial={{ opacity: 0, y: 50 }}
                 whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
                 transition={{
                   duration: 1,
                 }}
@@ -58,6 +64,7 @@ function PresidentSpeech() {
                 <motion.div
                   initial={{ opacity: 0, y: -30 }}
                   whileInView={{ opacity: 1, y: 0 }}
+                  viewport={{ once: true }}
                   transition={{ duration: 1 }}
                   className="flex flex-col gap-y-2"
                 >
@@ -85,13 +92,6 @@ function PresidentSpeech() {
           );
         })}
       </div>
-      {PresidentSect?.length === 0 ? (
-        <div className="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center ">
-          <Dots color="#b67a3d" size={40} speed={0.7} animating={true} />
-        </div>
-      ) : (
-        ""
-      )}
     </div>
   );
 }
