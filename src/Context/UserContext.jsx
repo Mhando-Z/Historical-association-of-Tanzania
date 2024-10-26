3d3a43e3625ac833ea5767a6409e816cbe28fd39
Mhando-Z <Mhandosz17@gmail.com>
2024-10-26 14:24:51 +0300

    Pase Two

--- src/Context/UserContext.jsx
+++ src/Context/UserContext.jsx
@@ -87,6 +87,8 @@ export function UserProvider({ children }) {
         setId,
         setShow,
         setUserData,
+        // functions
+        fetchUserData,
       }}
     >
       {children}
--- src/Context/axiosInstance.jsx
+++ src/Context/axiosInstance.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-  // baseURL: process.env.REACT_APP_API_URL,
-  baseURL: "http://127.0.0.1:8000/",
+  baseURL: process.env.REACT_APP_API_URL,
+  // baseURL: "http://127.0.0.1:8000/",
   // headers: {
   //   "Content-Type": "multipart/form-data",
   // },
